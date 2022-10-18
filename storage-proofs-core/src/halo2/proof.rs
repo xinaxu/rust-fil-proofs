8c7c2f37613a4e71e5181b829b13141d0bbfc3b1
Volker Mische <volker.mische@gmail.com>
2022-10-18 14:39:48 +0200

    chore: update deps

--- storage-proofs-core/src/halo2/proof.rs
+++ storage-proofs-core/src/halo2/proof.rs
@@ -35,7 +35,7 @@ type Challenge<C> = Challenge255<C>;
 type TranscriptReader<'proof, C> = Blake2bRead<&'proof [u8], C, Challenge<C>>;
 type TranscriptWriter<C> = Blake2bWrite<Vec<u8>, C, Challenge<C>>;
 
-pub trait Halo2Field: FieldExt + serde::Serialize {
+pub trait Halo2Field: FieldExt + serde::Serialize + serde::de::DeserializeOwned {
     type Curve: CurveExt<ScalarExt = Self>;
     type Affine: CurveAffine<ScalarExt = Self>;
 }
@@ -377,7 +377,7 @@ where
     C: CurveAffine,
     Circ: Circuit<C::Scalar> + CircuitRows,
     R: RngCore,
-    C::ScalarExt: serde::Serialize,
+    C::ScalarExt: serde::Serialize + serde::de::DeserializeOwned,
 {
     let pub_inputs: Vec<&[C::Scalar]> = pub_inputs.iter().map(Vec::as_slice).collect();
     let mut transcript = TranscriptWriter::init(vec![]);
@@ -403,7 +403,7 @@ where
     C: CurveAffine,
     Circ: Circuit<C::Scalar> + CircuitRows,
     R: RngCore,
-    C::ScalarExt: serde::Serialize,
+    C::ScalarExt: serde::Serialize + serde::de::DeserializeOwned,
 {
     assert_eq!(circuits.len(), pub_inputs.len());
     let pub_inputs: Vec<Vec<&[C::Scalar]>> = pub_inputs
