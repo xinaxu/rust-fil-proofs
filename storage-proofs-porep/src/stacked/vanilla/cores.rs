9ecf1f0d05ee47e80137310f47e32e5c3ea6b6da
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2022-11-19 22:36:17 -0800

    Fix logical processor

--- storage-proofs-porep/src/stacked/vanilla/cores.rs
+++ storage-proofs-porep/src/stacked/vanilla/cores.rs
@@ -2,7 +2,7 @@ use std::convert::TryInto;
 use std::sync::{Mutex, MutexGuard};
 
 use anyhow::{format_err, Result};
-use hwloc::{Bitmap, ObjectType, Topology, TopologyObject, CPUBIND_THREAD};
+use hwloc::{Bitmap, ObjectType, Topology, TopologyObject, CPUBIND_THREAD, CpuSet};
 use lazy_static::lazy_static;
 use log::{debug, warn};
 use storage_proofs_core::settings::SETTINGS;
@@ -79,25 +79,13 @@ pub fn bind_core(core_index: CoreIndex) -> Result<Cleanup> {
     let child_topo = &TOPOLOGY;
     let tid = get_thread_id();
     let mut locked_topo = child_topo.lock().expect("poisoned lock");
-    let core = get_core_by_index(&locked_topo, core_index)
-        .map_err(|err| format_err!("failed to get core at index {}: {:?}", core_index.0, err))?;
-
-    let cpuset = core
-        .allowed_cpuset()
-        .ok_or_else(|| format_err!("no allowed cpuset for core at index {}", core_index.0,))?;
-    debug!("allowed cpuset: {:?}", cpuset);
-    let mut bind_to = cpuset;
-
-    // Get only one logical processor (in case the core is SMT/hyper-threaded).
-    bind_to.singlify();
 
     // Thread binding before explicit set.
     let before = locked_topo.get_cpubind_for_thread(tid, CPUBIND_THREAD);
 
-    debug!("binding to {:?}", bind_to);
     // Set the binding.
     let result = locked_topo
-        .set_cpubind_for_thread(tid, bind_to, CPUBIND_THREAD)
+        .set_cpubind_for_thread(tid, CpuSet::from(core_index.0 as u32), CPUBIND_THREAD)
         .map_err(|err| format_err!("failed to bind CPU: {:?}", err));
 
     if result.is_err() {
